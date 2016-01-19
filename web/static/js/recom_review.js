a60ff9058af4a7c693750176db975a675545e7fb
Dmitriy Khodakov <dmitryhd@gmail.com>
2016-01-19 17:05:19 +0300

    Can now rotate users

--- web/static/js/recom_review.js
+++ web/static/js/recom_review.js
@@ -21,9 +21,19 @@ app.controller('reviewCtrl', function($scope, $http) {
             $scope.recoms = data.recoms;
             $scope.history = data.history;
             $scope.user_ids = data.user_ids;
-            console.log($scope.user_ids);
         });
     };
+    $scope.nextUser = function (incr) {
+        $scope.user_index += incr;
+        if ($scope.user_index >= $scope.user_ids.length) {
+            $scope.user_index = $scope.user_ids.length - 1;
+        }
+        if ($scope.user_index <= 0) {
+            $scope.user_index = 0;
+        }
+        $scope.user_id = $scope.user_ids[$scope.user_index];
+        $scope.loadRecommendations($scope.user_id);
+    }
     // at the bottom of your controller
     var init = function () {
        // check if there is query in url
